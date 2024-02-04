70817171b3a34c6a21bb65ad6c6d5dc952e282d2
tripathi08 <priyanshu.tri67@gmail.com>
2024-02-04 21:48:29 +0530

    completed sign up page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,9 @@ export default function Header() {
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3' >
       <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+        <Link to='/'>
         <span className='text-slate-500'>Estatify</span>
+        </Link>
       </h1>
       <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
         <input type='text' placeholder='Search...' 
