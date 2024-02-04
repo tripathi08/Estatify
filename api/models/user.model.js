cd4bcd684e727730a04b1d144f7958504deee00b
tripathi08 <priyanshu.tri67@gmail.com>
2024-02-04 19:46:24 +0530

    created signup api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,7 +15,7 @@ const userSchema= new mongoose.Schema({
     type: String,
     required: true,
    }
-},{timeseries:true});
+},{timestamps:true});
 
 const User=mongoose.model('User',userSchema);
 
