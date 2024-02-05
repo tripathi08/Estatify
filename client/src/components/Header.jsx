d2f2d05def0ceff9a163a2cea9ab53e66034f577
tripathi08 <priyanshu.tri67@gmail.com>
2024-02-05 16:52:36 +0530

    Completed profile page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3' >
       <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
         <Link to='/'>
-        <span className='text-slate-500'>Estatify</span>
+        <span className='text-slate-700 text-3xl'>Estatify</span>
         </Link>
       </h1>
       <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
