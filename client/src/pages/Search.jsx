cd88e2a19f7b6f79367a205984586b413f4ad2fe
tripathi08 <priyanshu.tri67@gmail.com>
2024-02-09 04:42:17 +0530

    created listing item card and show listings

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import ListingItem from '../components/ListingItem';
 
 export default function Search() {
     const navigate=useNavigate();
@@ -189,8 +190,21 @@ export default function Search() {
         >Search</button>
       </form>
       </div>
-      <div className=''>
+      <div className='flex-1'>
         <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>Listing Results</h1>
+        <div className='p-7 flex flex-wrap gap-4'>
+            {!loading && listings.length===0 &&(
+                <p className='text-xl text-slate-700 text-center w-full'>No listings found!</p>
+            )}
+            {loading && (
+                <p className='text-xl text-slate-700 text-center w-full'>Loading...</p>
+            )}
+            {
+                !loading && listings && listings.map((listing)=>
+                    <ListingItem key={listing._id} listing={listing}/>
+                )
+            }
+        </div>
       </div>
     </div>
   )
