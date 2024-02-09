2613dc83529a3cca5a514b871493239ddf332d71
tripathi08 <priyanshu.tri67@gmail.com>
2024-02-09 16:27:22 +0530

    completed about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      A
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold m-4'>About Estatify</h1>
+      <p className='m-4 text-slate-600 font-semibold'>At Estatify, we are committed to revolutionizing the real estate experience for our clients. With a passion for excellence and a dedication to providing exceptional service, we strive to be the leading destination for all your real estate needs. Whether you are buying, selling, or renting, our team of experienced professionals is here to guide you every step of the way.</p>
+      <h1 className='text-xl font-bold m-4'>Our Mission</h1>
+      <p className='m-4 text-slate-600 font-semibold'>Our mission at Estatify is simple: to empower our clients to make informed decisions about their real estate investments. We understand that buying or selling a property is one of the most significant transactions you will ever make, and we are here to ensure that your experience is seamless and stress-free. With our extensive knowledge of the market and commitment to transparency, you can trust us to help you achieve your real estate goals.</p>
+      <h1 className='text-xl font-bold m-4'>Why Choose Estatify?</h1>
+      <p className='m-4 text-slate-600 font-semibold'>What sets Estatify apart is our unwavering commitment to our clients' satisfaction. From personalized service to cutting-edge technology, we leverage every resource at our disposal to ensure that your experience with us exceeds your expectations. Whether you are a first-time homebuyer or a seasoned investor, you can count on us to provide you with the guidance and support you need to succeed in today's competitive real estate market. Choose Estatify and experience the difference for yourself.</p>
     </div>
   )
 }
