c00c359fc23fd8fe990b98b020a1819168dd6dc2
tripathi08 <priyanshu.tri67@gmail.com>
2024-02-09 15:08:17 +0530

    added show more listings functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -16,7 +16,7 @@ export default function Search() {
 
     const [loading,setLoading]=useState(false);
     const [listings,setListings]=useState([]);
-    console.log(listings);
+    const [showMore,setShowMore]=useState(false); 
 
     useEffect(()=>{
 
@@ -51,9 +51,16 @@ export default function Search() {
 
     const fetchListings = async () => {
         setLoading(true);
+        setShowMore(false);
         const searchQuery = urlParams.toString();
         const res = await fetch(`/api/listing/get?${searchQuery}`);
         const data = await res.json();
+        if(data.length>8){
+          setShowMore(true);
+        }
+        else{
+          setShowMore(false);
+        }
         setListings(data);
         setLoading(false);
       };
@@ -108,6 +115,20 @@ export default function Search() {
     urlParams.set('order', sideBarData.order);
     const searchQuery=urlParams.toString();
     navigate(`/search?${searchQuery}`);
+   }
+  
+   const onShowMoreClick=async ()=>{
+    const numberOfListings=listings.length;
+    const startIndex=numberOfListings;
+    const urlParams=new URLSearchParams(location.search);
+    urlParams.set('startIndex',startIndex);
+    const searchQuery=urlParams.toString();
+    const res=await fetch(`/api/listing/get?${searchQuery}`);
+    const data=await res.json();
+    if(data.length<9){
+      setShowMore(false);
+    }
+    setListings([...listings,...data]);
    }
 
   return (
@@ -204,6 +225,11 @@ export default function Search() {
                     <ListingItem key={listing._id} listing={listing}/>
                 )
             }
+            {showMore && (
+              <button onClick={onShowMoreClick}
+              className='text-green-700 hover:underline p-7 text-center w-full'
+              >Show More</button>
+            )}
         </div>
       </div>
     </div>
